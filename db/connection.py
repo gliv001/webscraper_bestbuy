0b8039ba065798f2d3a6d64bf0270161febcb25e
George <george.liv91@gmail.com>
2021-07-11 17:06:58 -0700

    removing unused code

--- db/connection.py
+++ db/connection.py
@@ -5,17 +5,6 @@ import yaml
 with open("config.yml", "r") as yml:
     cfg = yaml.safe_load(yml)
 
-# Connect to postgres database.
-engine = create_engine(cfg["db_connection"]["postgres"])
-# Bind session with the enginee. What database should it intereact is defined in bind parameter.
-Session = sessionmaker(bind=engine)
-# Initialize the session. To query against the db.
-session = Session()
-"""
-`declarative_base` is factory function that constructs a base class for declarative class definitions.
-SQL alchemy should know about the models we've defined. So we let it no by extending from Base.
-"""
-
 
 class DBSession:
     def __init__(self, db_uri: str = cfg["db_connection"]["postgres"]):
